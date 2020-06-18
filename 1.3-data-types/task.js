e2c4e85fc67ccea9740ad74d85bf4ad8472acf6e
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-06-18 23:32:41 +0600

    Solve 2 tasks

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,31 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+	if (isNaN(percent)) {
+		return `Параметр "Процентная ставка" содержит неправильное значение ${percent}`;
+	}
+	if (isNaN(contribution)) {
+		return `Параметр "Начальный взнос" содержит неправильное значение ${contribution}`;
+	}
+	if (isNaN(amount)) {
+		return `Параметр "Общая стоимость" содержит неправильное значение ${amount}`;
+	}
+
+	let loanBody = amount - contribution;
+	let term = (date.getFullYear() - new Date().getFullYear()) * 12 - (new Date().getMonth() - date.getMonth());
+	let percentPart = (percent / 100) / 12;
+
+	let payment = loanBody * (percentPart + percentPart / (Math.pow((1 + percentPart), term) - 1));
+	let totalAmount = payment * term;
+	console.log(parseFloat(totalAmount.toFixed(2)));
+
+    return parseFloat(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+	if (!name || name === null || name === undefined) {
+		name = "Аноним";
+	}
+	return `Привет, мир! Меня зовут ${name}`;
 }

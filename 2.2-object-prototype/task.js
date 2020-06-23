34d576b38f3dd5435d8ad56aa8f3c4e5f64981e0
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-06-23 10:22:08 +0600

    Solve 3 tasks

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,39 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+
+String.prototype.isPalindrome = function() {
+	let phrase = this.toLowerCase().split('');
+	for (let i = 0; i < phrase.length; i++) {
+		if (phrase[i] === ' ') {
+			phrase.splice(i, 1);
+		}
+	}
+	let counter = 0;
+	for (let i = 0; i < phrase.length; i++) {
+		if (phrase[i] === phrase[(phrase.length - 1) - i]) {
+			counter++;
+		}
+	}
+	if (counter === phrase.length) {
+		return true;
+	} else {
+		return false;
+	}	
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+	let sum = 0;
+	for (let mark of marks) {
+		sum += mark;
+	}
+	let average = sum / marks.length;
+	if (isNaN(average)) {
+		average = 0;
+	}
+	return Math.round(average);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+	let age = (new Date().getTime() - Date.parse(birthday)) / 31557600000;
+	return (age > 18) ? true : false;
 }

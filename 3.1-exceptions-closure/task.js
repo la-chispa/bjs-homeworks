92211bd2326fb6c9efd4201af7e6cf3570df5854
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-06-29 01:59:32 +0600

    Solve 2 tasks. Maybe

---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,61 @@
+// Задача 1
+parseCount = (data) => {
+	if (isNaN(Number.parseInt(data))){
+       throw new Error('Невалидное значение');
+	} 
+	return Number.parseInt(data);
+}
+
+validateCount = (data) => {
+    try {
+        const resultParse = parseCount(data);
+        return resultParse;
+    } catch(e) {
+        return e;
+    }
+}
+
+
+// Задача 2
+class Triangle {
+	constructor(a, b, c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	getPerimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	getArea() {			
+		const p = this.getPerimeter() / 2;
+		let s = (Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
+		return parseFloat(s);
+	}
+}
+
+class FakeTriangle {
+
+	getPerimeter() {
+		return 'Ошибка! Треугольник не существует';
+	}
+
+	getArea() {
+		return 'Ошибка! Треугольник не существует';
+	}
+}
+
+function getTriangle(a, b, c) {
+	try {
+		if (a + b <= c || b + c <= a || a + c <= b) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		} else {
+			const triangle = new Triangle(a, b, c);
+			return triangle;
+		}
+	} catch(e) {
+		const triangle = new FakeTriangle;
+		return triangle;
+	}
+}

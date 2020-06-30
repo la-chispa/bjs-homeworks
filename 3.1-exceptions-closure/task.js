d68b290b1e701514b57c93b00691f76b984f26be
Olga Belotserkovskaya <olla.chispa@gmail.com>
2020-07-01 00:54:39 +0600

    Solve. Truly

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -1,15 +1,15 @@
 // Задача 1
 parseCount = (data) => {
-	if (isNaN(Number.parseInt(data))){
+	const value = Number.parseInt(data); 
+	if (isNaN(value)) {
        throw new Error('Невалидное значение');
 	} 
-	return Number.parseInt(data);
+	return value;
 }
 
 validateCount = (data) => {
     try {
-        const resultParse = parseCount(data);
-        return resultParse;
+        return parseCount(data);
     } catch(e) {
         return e;
     }
@@ -19,9 +19,13 @@ validateCount = (data) => {
 // Задача 2
 class Triangle {
 	constructor(a, b, c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
+		if (a + b <= c || b + c <= a || a + c <= b) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		} else {
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
 	}
 
 	getPerimeter() {
@@ -37,20 +41,19 @@ class Triangle {
 
 function getTriangle(a, b, c) {
 	try {
-		if (a + b <= c || b + c <= a || a + c <= b) {
-			throw new Error('Треугольник с такими сторонами не существует');
-		} else {
-			const triangle = new Triangle(a, b, c);
-			return triangle;
-		}
+		return (new Triangle(a, b, c));
 	} catch(e) {
-		const triangle = {};
-		triangle.getArea = function() {
-			return 'Ошибка! Треугольник не существует';
+		return {
+			getArea: function() {return 'Ошибка! Треугольник не существует'},
+			getPerimeter: function() {return 'Ошибка! Треугольник не существует'}
+			// Со стрелочными функциями вылетает синтаксическая ошибка. 
+			//Вариант блочный ↓
+			// getArea: function = () => {return 'Ошибка! Треугольник не существует'},
+			// getPerimeter: function = () => {return 'Ошибка! Треугольник не существует'}
+			//Вариант краткий ↓
+			// getArea: function = () => 'Ошибка! Треугольник не существует',
+			// getPerimeter: function = () => 'Ошибка! Треугольник не существует'
+		
 		}
-		triangle.getPerimeter = function() {
-			return 'Ошибка! Треугольник не существует';
-		}
-		return triangle;
 	}
 }
